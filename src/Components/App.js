c162353f943eddde89f584fb4b2e2e0aca510b2f
MINJEONG KIM <mjkim@gmail.com>
2021-07-20 18:43:56 +0900

    global styles with styled-reset

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Router from "./Router";
-import Header from "./Header";
+import GlobalStyles from "./GlobalStyles";
 
 class App extends Component {
     render() {
         return (
                 <>
-                    <Router>
-                        <Header/>
-                    </Router>
+                    <Router />
+                    <GlobalStyles />
                 </>
             );
     }
