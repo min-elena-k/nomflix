9018423049dfe948f8d9b6285396fae0268d8a07
MINJEONG KIM <mj@MINJEONGs-MacBook-Pro.local>
2021-09-25 00:25:14 +0900

    axios add

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: "a324eedc55e4cfe4f9a55ed7cc8caf27",
+        language: "en-US"
+    }
+})
+
+api.get("tv/popular");
